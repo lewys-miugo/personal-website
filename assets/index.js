f819290e31607732d5d17ef8874084b159848d7b
lewys-miugo <lewiswambugu01@gmail.com>
2025-06-01 22:21:13 +0300

    Added nav & banner slides and their respective styles and JS

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+function toggleMenu() {
+    document.getElementById('navLinks').classList.toggle('show');
+}
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) slide.classList.add('active');
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 3000);
